059e7751596c23ceb0185f539bf95fbf91f5b7c3
varsha2611 <varshac@g.clemson.edu>
2018-11-04 22:33:01 -0500

    Parallel support 2

--- application.sh
+++ application.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env bash
+#PBS -N gnu-parallel-example
+#PBS -qsub -I -l  select=1:ncpus=20:mem=120gb,walltime=72:00:00
+
+module add gnu-parallel
+
+cd $PBS_O_WORKDIR
+
 Input_file = $1
 Params = $2
 python generate_inputs.py -i $1 -p $2
