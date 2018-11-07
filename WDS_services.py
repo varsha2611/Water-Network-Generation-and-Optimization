4987c41ca22823b5ea3409acd4bb93720f4e1feb
varsha2611 <varshac@g.clemson.edu>
2018-11-07 00:14:26 -0500

    fix error

--- WDS_services.py
+++ WDS_services.py
@@ -481,8 +481,10 @@ def assign_elevation(new_G, network_data, new_network_data):
 
     #initialize with zero
     for node in new_G.nodes():
-    #neglect zero position add additional indexing
         new_distribution.append(0)
+
+    #neglect zero position add additional indexing
+    new_distribution.append(0)
 
     #Assign initial random values
     for node in new_G.nodes():
