0b71cb3e7b050829dcdb8274b81186f4e83ddcd8
varsha2611 <varshac@g.clemson.edu>
2018-11-07 01:59:11 -0500

    fix error

--- WDS_services.py
+++ WDS_services.py
@@ -354,7 +354,7 @@ def assign_tanks_and_pumps(new_G, G, network_data, new_network_data,id=""):
         b=1.5
         for partition in Partitions:
             probability = random.uniform(0, b)
-            if probability > 0.5:
+            if probability > 0.5 or len(new_network_data['TANKS'] == 0):
                 for node in Partitions[partition]:
                     if new_G.degree(node) == 1:
                         new_network_data['TANKS'].append(node)
@@ -557,7 +557,7 @@ def has_solution(input_network,id=""):
 
         def __init__(self):
             super(my_mo_problem, self).__init__(self.nbOfPipes + 25 * self.nbOfPumps + 3 * self.nbOfTanks, 2, 1)
-            self.types[:] = [Real(0, 9)] * self.nbOfPipes + [Real(0, self.n_curves - 1)] * self.nbOfPumps + [Real(25, 100)] * self.nbOfTanks + [Real(25, 40)] * self.nbOfTanks + [Real(5, 10)]* self.nbOfTanks
+            self.types[:] = [[Real(0, 9)] * self.nbOfPipes + [Real(0, self.n_curves - 1)] * self.nbOfPumps + [Real(25, 100)] * self.nbOfTanks + [Real(25, 40)] * self.nbOfTanks + [Real(5, 10)]* self.nbOfTanks]
             self.constraints[:] = "<=0"
             self.directions[:] = Problem.MINIMIZE
             # self.function = mixed_type
